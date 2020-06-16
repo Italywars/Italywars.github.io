f27982e6eddc81ae780c7f6c616f37e595d81c05
scamacho59 <66191010+scamacho59@users.noreply.github.com>
2020-06-15 19:55:16 -0700

    Cloning old repo

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
-const joinGame_div = document.getElementById("join-game");
+function changePage(pageName) {
+    switch (pageName) {
+        case "join-game":
+            location.href = "game.html";
+            console.log("Hey you want to join a game");
+            break;
+        case "login":
+            location.href = "login.html";
+            console.log("Hey you want to login");
+            break;
+        case "rules":
+            location.href = "rules.html";
+            console.log("Hey you want to see the rules")
+            break;
+    }
+}
 
-joinGame_div.addEventListener('click', function() {
+function main() {
 
-})
+    const joinGame_div = document.getElementById("join-game");
+    const login_div = document.getElementById("login");
+    const rules_div = document.getElementById("rules");
 
-/*
-<button onclick="document.location = 'default.asp'">HTML Tutorial</button>
-*/
+
+    joinGame_div.addEventListener('click', function() {
+        changePage("join-game");
+    })
+
+    login_div.addEventListener('click', function() {
+        changePage("login");    
+    })
+
+    rules_div.addEventListener('click', function() {
+        changePage("rules");
+    })
+}
+
+main();
