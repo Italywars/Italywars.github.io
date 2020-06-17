597a7867857082cba76aea1b85a8e5dbd56a8773
scamacho59 <66191010+scamacho59@users.noreply.github.com>
2020-06-17 14:17:17 -0700

    Fixed page linking with folders

--- pages/game/game.js
+++ pages/game/game.js
@@ -17,14 +17,14 @@
 */
 function changePage(pageName) {
     switch (pageName) {
-        case "join-game":
-            location.href = "pages/game/game.html";
+        case "rules":
+            location.href = "../../pages/rules/rules.html";
             break;
         case "login":
-            location.href = "pages/login/login.html";
+            location.href = "../../pages/login/login.html";
             break;
-        case "rules":
-            location.href = "pages/rules/rules.html";
+        case "home":
+            location.href = "../../index.html";
             break;
     }
 }
@@ -32,13 +32,13 @@ function changePage(pageName) {
 function main() {
 
     // Access information in index.html
-    const joinGame_div = document.getElementById("join-game");
-    const login_div = document.getElementById("login");
     const rules_div = document.getElementById("rules");
+    const login_div = document.getElementById("login");
+    const home_div = document.getElementById("home");
 
-    joinGame_div.addEventListener('click', () => changePage("join-game"));
-    login_div.addEventListener('click', () => changePage("login"));
     rules_div.addEventListener('click', () => changePage("rules"));
+    login_div.addEventListener('click', () => changePage("login"));
+    home_div.addEventListener('click', () => changePage("home"));
 
     // Just for fun. This isn't actually necessary.
     return 0;
