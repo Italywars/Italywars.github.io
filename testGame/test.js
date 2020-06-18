c698236b22d197863ed32025bc119ee65528041c
scamacho59 <66191010+scamacho59@users.noreply.github.com>
2020-06-17 17:21:19 -0700

    Changed stuff

--- testGame/test.js
+++
@@ -1,8 +0,0 @@
-/**
- * Test making a grid, and drawing an arrow
- * between different places on the grid.
- * Additionally, try to get a side view
- * demonstrating which boxes have been selected.
- */
-
-console.log('Hello, World!');
